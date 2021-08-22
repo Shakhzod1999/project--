a69439ff7fc04653c6084a1143d282bd7f7b9a2b
shakhzod1999 <shahzodtoyirov99@gmail.com>
2021-08-22 11:42:54 +0500

    projectКорзина

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+  let product = document.querySelectorAll(".product"),
+  buttons = document.querySelectorAll("button"),
+  openBtn = document.querySelector(".open");
+
+
+  function createCart() {
+    let cart = document.createElement('div'),
+      field = document.createElement('div'),
+      heading = document.createElement('h2'),
+      closeBtn = document.createElement('button');
+
+    cart.classList.add('cart')
+    field.classList.add('cart-field')
+    closeBtn.classList.add('close')
+
+
+    heading.textContent = 'В нашей корзина'
+    closeBtn.textContent = 'Закрыть'
+
+
+    document.body.appendChild(cart);
+    cart.appendChild(heading);
+    cart.appendChild(field);
+    cart.appendChild(closeBtn);
+
+  }
+
+  createCart();
+
+
+  let cart = document.querySelector(".cart"),
+      closeBtn = document.querySelector(".close"),
+      field = document.querySelector(".cart-field");
+
+
+  openBtn.addEventListener("click" , () => {
+    cart.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", () =>{
+    cart.style.display = "none";
+  });
+
+
+  buttons.forEach( (item,i) => {
+    item.addEventListener("click" , () => {
+      let cloneItem = product[i].cloneNode(true),
+          btn = cloneItem.querySelector('button');
+
+
+      btn.remove()
+      field.appendChild(cloneItem)
+      product[i].remove()
+    })
+  })
+
+
